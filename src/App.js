2478d5db4d55afd19e21e1e5579fa801aebbe6ef
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-03-02 12:40:23 +0100

    Dodat hero uradjen

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Navbar } from './pages/Navbar';
 import { Home } from './pages/Home';
 import { ErrorPage } from './pages/ErrorPage';
+import { Gallery } from './pages/Gallery';
+import Headroom from 'react-headroom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Headroom>
+          <Navbar />
+        </Headroom>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/galerija' element={<Gallery/>} />
           <Route path='*' element={<ErrorPage />} />
         </Routes>
       </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import heroImg from "../images/weddStar.jpeg"
 export const Home = () => {
     return(
         <div id="homePage">
             <div id="hero">
-                
+                <div id="heroText">
+                    <h1>Ne čekajte savršene trenutke. Uhvatite trenutke koji dolaze i učinite ih savršenim.</h1>
+                    <button>
+                        <span>Zakaži Odmah</span>
+                    </button>
+                </div>
+                <div id="heroImg">
+                    <img src={require("../images/weddStar.jpeg")} alt="Hero Img" />
+                </div>
             </div>
+            {/* End hero */}
+            
         </div>
     )
 }
